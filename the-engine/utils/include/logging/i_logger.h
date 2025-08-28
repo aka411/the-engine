8eaa28e1438e098ccafe45e1b9f18267ebea8b59
Akash Joshi <181660723+aka411@users.noreply.github.com>
2025-08-28 12:42:38 +0530

    new design for ILogger
    instead of using enums for log level its better to use dedicated methods, this is done mainly to work with macros to disable "debug" and "info" levels for production code.

--- the-engine/utils/include/logging/i_logger.h
+++ the-engine/utils/include/logging/i_logger.h
@@ -8,19 +8,32 @@ namespace TheEngine::Utils
 {
 
 
-class ILogger {
-    
-public:
-    virtual ~ILogger() = default;
-
-    // Core logging method
-    virtual void Log(const LogLevel level,const LogSource logSource , const std::string& message,
-                     const std::source_location location = std::source_location::current()) = 0;
+ class ILogger {
+    public:
+        virtual ~ILogger() = default;
 
 
-};
+        virtual void trace(const LogSource logSource, const std::string& message,
+                           const std::source_location location = std::source_location::current()) = 0;
+
+        virtual void debug(const LogSource logSource, const std::string& message,
+                           const std::source_location location = std::source_location::current()) = 0;
+
+        virtual void info(const LogSource logSource, const std::string& message,
+                          const std::source_location location = std::source_location::current()) = 0;
+
+        virtual void warn(const LogSource logSource, const std::string& message,
+                          const std::source_location location = std::source_location::current()) = 0;
+
+        virtual void error(const LogSource logSource, const std::string& message,
+                           const std::source_location location = std::source_location::current()) = 0;
+
+        virtual void fatal(const LogSource logSource, const std::string& message,
+                           const std::source_location location = std::source_location::current()) = 0;
+    };
 
 
 }
+
 
 
