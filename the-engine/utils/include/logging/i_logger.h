0f0430f5431eb2b197a322fe71ba3d1d8c403c17
Akash Joshi <181660723+aka411@users.noreply.github.com>
2025-08-28 13:53:20 +0530

    fix : pass message string by value message string was passed by reference and is corrected to pass by value. This is done to avoid dangling references and allow safe multithreaded operation of logger in future if design uses multithreading for logger.

--- the-engine/utils/include/logging/i_logger.h
+++ the-engine/utils/include/logging/i_logger.h
@@ -13,22 +13,22 @@ namespace TheEngine::Utils
         virtual ~ILogger() = default;
 
 
-        virtual void trace(const LogSource logSource, const std::string& message,
+        virtual void trace(const LogSource logSource, const std::string message,
                            const std::source_location location = std::source_location::current()) = 0;
 
-        virtual void debug(const LogSource logSource, const std::string& message,
+        virtual void debug(const LogSource logSource, const std::string message,
                            const std::source_location location = std::source_location::current()) = 0;
 
-        virtual void info(const LogSource logSource, const std::string& message,
+        virtual void info(const LogSource logSource, const std::string message,
                           const std::source_location location = std::source_location::current()) = 0;
 
-        virtual void warn(const LogSource logSource, const std::string& message,
+        virtual void warn(const LogSource logSource, const std::string message,
                           const std::source_location location = std::source_location::current()) = 0;
 
-        virtual void error(const LogSource logSource, const std::string& message,
+        virtual void error(const LogSource logSource, const std::string message,
                            const std::source_location location = std::source_location::current()) = 0;
 
-        virtual void fatal(const LogSource logSource, const std::string& message,
+        virtual void fatal(const LogSource logSource, const std::string message,
                            const std::source_location location = std::source_location::current()) = 0;
     };
 
