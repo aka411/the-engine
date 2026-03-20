9578d7728967d3eccf4ba448180ccc8543559e83
aka411 <181660723+aka411@users.noreply.github.com>
2026-03-20 19:00:21 +0530

    chore : remove old files from old design

--- include/rendering-engine/vulkan-backend/common_data_types.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <glm/ext/matrix_float4x4.hpp>
-
-struct PushConst
-{
-	glm::mat4 modelMatrix;
-	uint64_t ssboOffset = 0;
-};
--- include/rendering-engine/vulkan-backend/utlis.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-
-
-static void logger(const char* msg)
-{
-	std::cout << msg << std::endl;
-}
