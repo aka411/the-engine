eb4134395108b93e3fc6fa4fe32477ed617d3ccb
aka411 <181660723+aka411@users.noreply.github.com>
2026-07-10 21:59:59 +0530

    refactor :  add methods for window resize handling and frame sync

--- include/rendering-system/rhi/i_presentation_system.h
+++ include/rendering-system/rhi/i_presentation_system.h
@@ -5,6 +5,7 @@ namespace TheEngine::RenderingSystem
 {
 	class ICommandBuffer;
 
+
 	class IPresentationSystem
 	{
 	
@@ -19,15 +20,19 @@ namespace TheEngine::RenderingSystem
 
 		virtual ~IPresentationSystem() = default;
 	
+		virtual void startFrame() = 0 ;
+
 		virtual const int getCurrentFrameIndex() const = 0;
 
 		virtual ICommandBuffer& getRenderCommandBuffer() = 0;
 
 
+		virtual void setWindowSize(const WindowExtent& windowExtent) = 0;
+
 			//Implcity know which frame its from 
 		virtual void submitRenderCommandBuffer(ICommandBuffer& commandBuffer) = 0;
 
-
+		virtual void endFrame() = 0;
 
 	};
 
