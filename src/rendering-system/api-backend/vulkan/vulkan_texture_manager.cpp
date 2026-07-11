e077b610cde1843d780a02045c3469caacb6b987
aka411 <181660723+aka411@users.noreply.github.com>
2026-07-12 03:58:28 +0530

    feat : add vulkan texture manager

--- src/rendering-system/api-backend/vulkan/vulkan_texture_manager.cpp
+++ src/rendering-system/api-backend/vulkan/vulkan_texture_manager.cpp
@@ -1,33 +1,80 @@
 
+#include <rendering-system/api-backend/vulkan/vulkan_texture_manager.h>
+#include <rendering-system/api-backend/vulkan/vulkan_transfer_manager.h>
+#include <rendering-system/api-backend/vulkan/utils/engine_to_vulkan_types.h>
+
+
+
+
 namespace TheEngine::RenderingSystem::VulkanBackend
 {
 
 
 
-	VulkanTextureManager::VulkanTextureManager()
+
+	VulkanTextureManager::VulkanTextureManager(VulkanTextureStore& vulkanTextureStore, VulkanTransferManager& vulkanTransferManager) :
+		m_vulkanTextureStore(vulkanTextureStore),
+		m_vulkanTransferManager(vulkanTransferManager)
 	{
+
+
+
+
+
 
 	}
 
-	virtual VulkanTextureManager::~VulkanTextureManager() override
+	VulkanTextureManager::~VulkanTextureManager()
 	{
+		//TODO : IMPLEMENT THIS
+	}
+
+    TextureHandle VulkanTextureManager::createNewTexture(TextureCreateInfo& textureCreateInfo)
+	{
+
+
+		const TextureHandle textureHandle = m_vulkanTextureStore.createNewTexture(textureCreateInfo,m_textureRegistry);
+
+		VkImage vkImage = m_vulkanTextureStore.getVulkanTexture(textureHandle).vkImage;
+
+       
+        if (textureCreateInfo.memoryBlock.getData() != nullptr)
+        {
+            ImageTransferRequest transferRequest(std::move(textureCreateInfo.memoryBlock), textureCreateInfo.desc);
+     
+
+            this->m_vulkanTransferManager.transferToImage(std::move(transferRequest), vkImage);
+        }
+
+   
+
+        return textureHandle;
 
 	}
 
-	virtual TextureHandle VulkanTextureManager::createNewTexture(const TextureCreateInfo& textureCreateInfo) override
+	void VulkanTextureManager::destroyTexture(const TextureHandle& textureHandle)
 	{
+
+
+		//TODO : IMPLEMENT THIS
+
 
 	}
 
-	virtual void VulkanTextureManager::destroyTexture(const TextureHandle& textureHandle) override
+
+
+
+	VkDescriptorSet VulkanTextureManager::getBindlessImageVkDescriptorSet()
 	{
+
+		return m_vulkanTextureStore.getBindlessImageVkDescriptorSet();
 
 	}
 
-
-
-
-
+    VulkanTexture VulkanTextureManager::getVulkanTexture(const TextureHandle& textureHandle)
+    {
+		return m_vulkanTextureStore.getVulkanTexture(textureHandle);
+    }
 
 
 }
