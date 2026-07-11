f722ecfad5ce1345e01dd2dd1eb3dcebb4c0bd51
aka411 <181660723+aka411@users.noreply.github.com>
2026-07-12 03:55:27 +0530

    feat : add vulkan texture manager

--- include/rendering-system/api-backend/vulkan/vulkan_texture_manager.h
+++ include/rendering-system/api-backend/vulkan/vulkan_texture_manager.h
@@ -1,28 +1,45 @@
 #pragma once
+#include <rendering-system/rhi/i_texture_manager.h>
+#include <rendering-system/rhi/data-structures/gpu_texture_data_structures.h>
+
+#include <rendering-system/api-backend/vulkan/vulkan-only/vulkan_texture_store.h>
+
+
+
 
 namespace TheEngine::RenderingSystem::VulkanBackend
 {
+	class VulkanTransferManager;
+	class VulkanTextureStore;
+
+
+
 
 	class VulkanTextureManager : public ITextureManager
 	{
 	private:
 
+		VulkanTextureStore& m_vulkanTextureStore;
+		VulkanTransferManager& m_vulkanTransferManager;
+
 	public:
 
 
 
-		VulkanTextureManager();
+		VulkanTextureManager(VulkanTextureStore& vulkanTextureStore,VulkanTransferManager& vulkanTransferManager);
 
 		virtual ~VulkanTextureManager() override;
 
-		virtual TextureHandle createNewTexture(const TextureCreateInfo& textureCreateInfo) override;
+		virtual TextureHandle createNewTexture(TextureCreateInfo& textureCreateInfo) override;
 
 		virtual void destroyTexture(const TextureHandle& textureHandle) override;
 
+
+
+		/*NATIVE METHODS*/
+		VkDescriptorSet getBindlessImageVkDescriptorSet();
+		VulkanTexture getVulkanTexture(const TextureHandle& textureHandle);
 	};
-
-
-
 
 
 
