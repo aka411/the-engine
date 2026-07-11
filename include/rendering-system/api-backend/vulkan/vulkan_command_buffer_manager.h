7984a84a23299719d36814a136182642a18fed4b
aka411 <181660723+aka411@users.noreply.github.com>
2026-07-12 01:07:42 +0530

    feat : add vulkan command buffer manager

--- include/rendering-system/api-backend/vulkan/vulkan_command_buffer_manager.h
+++ include/rendering-system/api-backend/vulkan/vulkan_command_buffer_manager.h
@@ -1,1 +1,52 @@
-VkCommandPool m_commandPool;
+#pragma once
+#include <map>
+
+#include <volk.h>
+#include <rendering-system/rhi/i_command_buffer_manager.h>
+#include <rendering-system/api-backend/vulkan/vulkan_command_buffer.h>
+#include <rendering-system/api-backend/vulkan/vulkan-only/vullkan_queue_manager.h>
+
+
+
+
+
+
+namespace TheEngine::RenderingSystem::VulkanBackend
+{
+
+	class VulkanContext;
+	class VulkanDescriptorSetManager;
+	//class VulkanQueueManager;
+
+	class VulkanCommandBufferManager //: ICommandBufferManager
+	{
+
+	private:
+
+		VulkanContext& m_vulkanContext;
+		VulkanQueueManager& m_vulkanQueueManager;
+
+		std::map<QueueType, VkCommandPool > m_commandPools;
+
+		VulkanResourceResolver& m_vulkanResourceResolver;
+		VulkanDescriptorSetManager& m_vulkanDescriptorSetManager;
+
+	public:
+
+		VulkanCommandBufferManager(VulkanContext& vulkanContext, VulkanQueueManager& vulkanQueueManager, VulkanResourceResolver& vulkanResourceResolver, VulkanDescriptorSetManager& vulkanDescriptorSetManager);
+
+
+		VulkanCommandBuffer getCommandBuffer(TheEngine::RenderingSystem::VulkanBackend::QueueType queueType);
+
+
+	};
+
+
+
+
+
+
+
+
+
+}
