eb3f51012fb32cf4d178acd386f62845c190f000
aka411 <181660723+aka411@users.noreply.github.com>
2026-07-12 21:47:18 +0530

    feat : add vulkan descriptor set manager

--- include/rendering-system/api-backend/vulkan/vulkan-only/vulkan_descriptor_set_manager.h
+++ include/rendering-system/api-backend/vulkan/vulkan-only/vulkan_descriptor_set_manager.h
@@ -1,19 +1,69 @@
 #pragma once
+#include <array>
+#include <unordered_map>
+
+#include <volk.h>
+
+#include <rendering-system/rhi/data-structures/descriptor_set_layout.h>
+
+
+
 
 namespace TheEngine::RenderingSystem::VulkanBackend
 {
+	
+
+	class VulkanContext;
+
+
+
+
+	constexpr uint32_t NUMBER_OF_FRAMES_IN_FLIGHT = 2;//Need to move it to a central location like context maybe
+	enum class DescriptorPoolType
+	{
+		STATIC_POOL,
+		DYNAMIC_POOL
+	};
 
 	class VulkanDescriptorSetManager 
 	{
 
 	private:
-		
-		VkDescriptorPool m_descriptorPool;
+
+		VulkanContext& m_vulkanContext;
+
+		//State
+		uint32_t m_currentFrameIndex = 0;
+
+		//POOLS
+		VkDescriptorPool m_staticVkDescriptorPool;
+		std::array<VkDescriptorPool, NUMBER_OF_FRAMES_IN_FLIGHT> m_perFrameVkDescriptorPool;
+
+
+		std::unordered_map<uint64_t,VkDescriptorSetLayout> m_cachedVkDescriptorSetLayouts;
+
+
+	private:
+
+		VkDescriptorSetLayout createVkDescriptorSetLayout(DescriptorSetLayout& descriptorSetLayout);
+
+		void createVkDescriptorPool(VkDescriptorPoolCreateInfo& vkDescriptorPoolCreateInfo, VkDescriptorPool& vkDescriptorPool);
+
+		void resetDynamicPool(const uint32_t frameIndex);
 
 	public:
-		VulkanDescriptorSetManager();
+
+		VulkanDescriptorSetManager(VulkanContext& vulkanContext);
 
 
+
+		VkDescriptorSetLayout getOrCreateVkDescriptorSetLayout(DescriptorSetLayout& descriptorSetLayout);
+		VkDescriptorSet createVkDescriptorSet(const VkDescriptorSetLayout& vDescriptorSetLayout, DescriptorPoolType descriptorPoolType);
+
+
+		//State Management
+		void setCurrentFrame(const uint32_t frameIndex);
+		
 	};
 
 
