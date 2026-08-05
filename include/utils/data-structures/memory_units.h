232a9417775eb99a545cca2a688c755966b4e242
aka411 <181660723+aka411@users.noreply.github.com>
2026-08-05 21:16:41 +0530

    feat : add memory size constants

---
+++ include/utils/data-structures/memory_units.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <cstddef>
+
+
+namespace TheEngine
+{
+    constexpr size_t Byte{ 1 };
+    constexpr size_t KiB{ 1024 * Byte };
+    constexpr size_t MiB{ 1024 * KiB };
+    constexpr size_t GiB{ 1024 * MiB };
+}
